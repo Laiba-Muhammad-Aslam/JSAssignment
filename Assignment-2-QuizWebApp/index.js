b69f27a2c14eb77971bd8ca381cac8b2dd93ae30
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-08-15 21:58:29 +0500

    markup for quiz

---
+++ Assignment-2-QuizWebApp/index.js
@@ -0,0 +1,69 @@
+var question1 = prompt("Which property is used to change the size of text in CSS?");
+if( question1 === "font-size"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question2 = prompt("What does HTML stand for?");
+if( question2 === "Hyper Text Markup Language"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question3 = prompt("Which HTML tag is used to create a paragraph?");
+if( question3 === "<p>"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question4 = prompt("Which tag is used to display an image in HTML?");
+if( question4 === "<img>"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question5 = prompt("What does CSS stand for?");
+if( question5 === "Cascading Style Sheets"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question6 = prompt("Which property is used to change the background color of an element in CSS?");
+if( question6 === "background-color"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question7 = prompt("Which CSS property is used to change the text color of an element?");
+if( question7 === "color"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question8 = prompt("Which is the correct way to write a JavaScript alert box?");
+if( question8 === "alert()"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question9 = prompt("Which tag is used to create a table in HTML?");
+if( question9 === "<table>"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
+
+var question10 = prompt("What is the correct HTML tag for a line break?");
+if( question10 === "<br>"){
+    alert("Correct");
+}else{
+    alert("Wrong!!");
+}
