555416212665db56303e00ccc0e597ae085aed2b
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-29 18:43:16 +0500

    objects of a players

---
+++ Assignment-6/ObjectsAssignment/index.js
@@ -0,0 +1,100 @@
+var player1 = {
+    name: "Cristiano Ronaldo",
+    fatherName: "José Dinis Aveiro",
+    dateOfBirth: "5 February 1985",
+    nationality: "Portuguese",
+    height: 76 + "cm",
+    playerShirtNumber: 7,
+    socialMediaLinks: "@cristiano"
+}
+
+var player2 = {
+    name: "Lionel Messi",
+    fatherName: "Jorge Messi",
+    dateOfBirth: "24 June 1987",
+    nationality: "Argentine",
+    height: 70 + "cm",
+    playerShirtNumber: 10,
+    socialMediaLinks: "@leomessi"
+}
+
+var player3 = {
+    name: "Neymar Jr",
+    fatherName: "Neymar Santos Sr",
+    dateOfBirth: "5 February 1992",
+    nationality: "Brazilian",
+    height: 75 + "cm",
+    playerShirtNumber: 10,
+    socialMediaLinks: "@neymarjr"
+}
+
+var player4 = {
+    name: "Kylian Mbappé",
+    fatherName: "Wilfried Mbappé",
+    dateOfBirth: "20 December 1998",
+    nationality: "French",
+    height:  78 + "cm",
+    playerShirtNumber: 7,
+    socialMediaLinks: "@k.mbappe"
+}
+
+var player5 = {
+    name: "Mohamed Salah",
+    fatherName: "Salah Ghaly",
+    dateOfBirth: "15 June 1992",
+    nationality: "Egyptian",
+    height: 75 + "cm",
+    playerShirtNumber: 11,
+    socialMediaLinks: "@mosalah"
+}
+
+var player6 = {
+    name: "Kevin De Bruyne",
+    fatherName: "Herwig De Bruyne",
+    dateOfBirth: "28 June 1991",
+    nationality: "Belgian",
+    height:  81 + "cm",
+    playerShirtNumber: 17,
+    socialMediaLinks: "@kevindebruyne"
+}
+
+var player7 = {
+    name: "Robert Lewandowski",
+    fatherName: "Krzysztof Lewandowski",
+    dateOfBirth: "21 August 1988",
+    nationality: "Polish",
+    height: 85 + "cm",
+    playerShirtNumber: 9,
+    socialMediaLinks: "@_rl9"
+}
+
+var player8 = {
+    name: "Sergio Ramos",
+    fatherName: "José María Ramos",
+    dateOfBirth: "30 March 1986",
+    nationality: "Spanish",
+    height: 84 + "cm",
+    playerShirtNumber: 4,
+    socialMediaLinks: "@sergioramos"
+}
+
+var player9 = {
+    name: "Virgil van Dijk",
+    fatherName: "Ron van Dijk",
+    dateOfBirth: "8 July 1991",
+    nationality: "Dutch",
+    height: 93 + "cm",
+    playerShirtNumber: 4,
+    socialMediaLinks: "@virgilvandijk"
+}
+
+var player10 = {
+    name: "Erling Haaland",
+    fatherName: "Alf-Inge Haaland",
+    dateOfBirth: "21 July 2000",
+    nationality: "Norwegian",
+    height: 94 + "cm",
+    playerShirtNumber: 9,
+    socialMediaLinks: "@erling.haaland"
+}
+// console.log(player1.height);
